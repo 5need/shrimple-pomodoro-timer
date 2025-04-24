e7ca4103daa49d10154116b333fbac6ade3df1a0
mccullen <mccullen@mccullen.xyz>
2025-04-24 14:23:58 -0500

    move over to a simpler python http server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
-export ENV=development
-
-air
-
-wait
+tailwindcss -i ./input.css -o ./public/output.css --watch &
+python3 -m http.server 3000 --directory ./public &
+browser-sync start --proxy localhost:3000 --files './public/*.html,./public/*.css,./public/*.js' --no-ui --no-open --port 3001
--- go.mod
+++
@@ -1,5 +0,0 @@
-module shrimple-pomodoro-timer
-
-go 1.23.5
-
-require github.com/a-h/templ v0.3.857
